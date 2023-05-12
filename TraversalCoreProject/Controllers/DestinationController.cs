716aba52f4ee512d244ca9b722662b0b1983a15a
barisbukumculer <126320079+barisbukumculer@users.noreply.github.com>
2023-05-12 03:17:35 +0300

    25

--- TraversalCoreProject/Controllers/DestinationController.cs
+++ TraversalCoreProject/Controllers/DestinationController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
 
 namespace TraversalCoreProject.Controllers
 {
@@ -9,8 +11,20 @@ namespace TraversalCoreProject.Controllers
         DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
         public IActionResult Index()
         {
-            var values=destinationManager.TGetList();
+            var values = destinationManager.TGetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            ViewBag.i = id;
+            var values=destinationManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination destination)
+        {
+            return View();
         }
     }
 }
