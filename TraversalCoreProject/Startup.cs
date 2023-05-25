fe53458e4f5c73b74b6abaff908fea84edd97f4b
barisbukumculer <126320079+barisbukumculer@users.noreply.github.com>
2023-05-26 02:16:47 +0300

    50

--- TraversalCoreProject/Startup.cs
+++ TraversalCoreProject/Startup.cs
@@ -77,6 +77,14 @@ namespace TraversalCoreProject
                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                 );
             });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
